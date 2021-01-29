ea0e2ede915ef5fe050eeba9f1c50962e17d18e0
jA0604 <j1107@bk.ru>
2021-01-29 18:05:56 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/dz_1_1_and_kt/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.netology.dz_1_1_and_kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
